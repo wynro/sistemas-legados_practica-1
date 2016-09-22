83601ca57300fc65b7dbdabac7bd7b2a844e8f41
Guillermo Robles <guillerobles1995@gmail.com>
2016-09-22 19:35:25 +0200

    Change program to free form
    - Update all source files, eliminating unnecessary indentation and
      unwrapping lines
    - Update Makefile to use the compiler's --free option

--- src/main.cob
+++ src/main.cob
@@ -1,32 +1,28 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. MAINHRMS.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MAINHRMS.
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+    77 CHOICE PIC 9.
 
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       77 CHOICE PIC 9.
-                                        
-       PROCEDURE DIVISION.
-       MAIN-PARA.
-           DISPLAY SPACES AT 0101 WITH ERASE EOS.
-           DISPLAY "*******************************************"
-                AT 0315.
-           DISPLAY "     HUMAN RESOURCE MANAGEMENT SYSTEM      "
-                AT 0515.
-           DISPLAY "*******************************************"
-                AT 0715.
-           DISPLAY "1. HRMS WRITE" AT 1025.
-           DISPLAY "2. HRMS READ" AT 1225.
-           DISPLAY "3. EXIT" AT 1425.
-           DISPLAY "ENTER YOUR CHOICE :" AT 1625.
-           ACCEPT CHOICE AT 1646.
-           IF CHOICE = 1
-               CALL "EMPWRITE"
-               CANCEL "EMPWRITE"
-               GO TO MAIN-PARA
-           ELSE
-               IF CHOICE = 2
-                   CALL "EMPREAD"
-                   CANCEL "EMPREAD"
-                   GO TO MAIN-PARA
-               ELSE
-                   STOP RUN.
+PROCEDURE DIVISION.
+MAIN-PARA.
+    DISPLAY SPACES AT 0101 WITH ERASE EOS.
+    DISPLAY "*******************************************" AT 0315.
+    DISPLAY "     HUMAN RESOURCE MANAGEMENT SYSTEM      " AT 0515.
+    DISPLAY "*******************************************" AT 0715.
+    DISPLAY "1. HRMS WRITE" AT 1025.
+    DISPLAY "2. HRMS READ" AT 1225.
+    DISPLAY "3. EXIT" AT 1425.
+    DISPLAY "ENTER YOUR CHOICE :" AT 1625.
+    ACCEPT CHOICE AT 1646.
+    IF CHOICE = 1
+        CALL "EMPWRITE"
+        CANCEL "EMPWRITE"
+        GO TO MAIN-PARA
+    ELSE
+       IF CHOICE = 2
+           CALL "EMPREAD"
+           CANCEL "EMPREAD"
+           GO TO MAIN-PARA
+       ELSE
+           STOP RUN.
